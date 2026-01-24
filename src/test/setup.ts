de7a8d8edb1cedcad91ed2e3422f1888951b99d0
toliveira1802-sketch <toliveira1802@gmail.com>
2026-01-24 18:41:50 -0300

    Refactor setup file for Vitest configuration

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,15 +1,16 @@
-import "@testing-library/jest-dom";
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-Object.defineProperty(window, "matchMedia", {
-  writable: true,
-  value: (query: string) => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: () => {},
-    removeListener: () => {},
-    addEventListener: () => {},
-    removeEventListener: () => {},
-    dispatchEvent: () => {},
-  }),
-});
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    css: true,
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
