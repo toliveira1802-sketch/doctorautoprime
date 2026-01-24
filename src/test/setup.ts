9612f8fd4c3f761e9dce49b420e05b314e01c9b7
toliveira1802-sketch <toliveira1802@gmail.com>
2026-01-24 18:42:13 -0300

    Add testing-library for improved testing support

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,16 +1,1 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-    css: true,
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+import '@testing-library/jest-dom'
